4d844fbc4e14ec738718429e4940fd5d706db100
Scusi <scusi@snurn.de>
2016-04-29 21:42:15 +0200

    added some basic help comment

--- Examples/GetSampleInfo/vmrGetSampleInfo.go
+++ Examples/GetSampleInfo/vmrGetSampleInfo.go
@@ -1,3 +1,5 @@
+// vmrGetSampleInfo.go - takes a vmray sampleId, provides Information about the sample
+//
 package main
 
 import (
@@ -6,12 +8,11 @@ import (
 	"github.com/scusi/vmray"
 	"log"
 	"os"
-	//"net/http"
-	//"crypto/tls"
 	"flag"
 )
 
-var sample_id string // sample_id
+// sample_id variable
+var sample_id string 
 
 func init() {
 	flag.StringVar(&sample_id, "sample_id", "", "ID of Sample to retrieve Infos for")
@@ -25,20 +26,12 @@ func check(err error) {
 
 func main() {
 	flag.Parse()
-	//tr := &http.Transport{
-	//TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	//}
-	//httpclient := &http.Client{Transport: tr}
 	c, err := vmray.New(
-		//vmray.SetUrl("https://live.vmray.com/api/"),
 		vmray.SetErrorLog(log.New(os.Stderr, "vmray error: ", log.Lshortfile)),
 		//vmray.SetTraceLog(log.New(os.Stderr, "vmray trace: ", log.Lshortfile)),
 		vmray.SetBasicAuth(os.Getenv("VMRAY_EMAIL"), os.Getenv("VMRAY_PASSWD")),
-		//vmray.SetHttpClient(httpclient),
-		//vmray.SetHttpClient(&http.Client{}),
 	)
 	check(err)
-	//fmt.Printf("vmray client: %#v\n", c)
 
 	f, err := c.GetSampleInfo(sample_id)
 	check(err)
